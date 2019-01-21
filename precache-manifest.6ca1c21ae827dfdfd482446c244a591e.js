5c1382dabafde610c417125dc7dc279e589ce0bb
Hemanth Vakulabharanam <hemanthv@Hemanths-MBP.frontierlocal.net>
2019-01-21 15:20:28 -0600

    Updates

---
+++ precache-manifest.6ca1c21ae827dfdfd482446c244a591e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "adf7548112fd866dd157e833b47d79e7",
+    "url": "/portfolio/static/media/skype_profile.adf75481.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "47451bdc372e46d63523",
+    "url": "/portfolio/static/js/main.47451bdc.chunk.js"
+  },
+  {
+    "revision": "9f842e1f2ebb710b569c",
+    "url": "/portfolio/static/js/1.9f842e1f.chunk.js"
+  },
+  {
+    "revision": "47451bdc372e46d63523",
+    "url": "/portfolio/static/css/main.5843e451.chunk.css"
+  },
+  {
+    "revision": "bbf49a76a54fa96b29fe77fe6c00306b",
+    "url": "/portfolio/index.html"
+  }
+];
